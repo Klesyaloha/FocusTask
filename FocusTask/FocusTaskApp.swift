3f14cecfb4e80e07847e86ad05e8357a42619a36
Klesya <v.klesya@outlook.fr>
2024-09-12 21:52:10 +0200

    Initial Commit

---
+++ FocusTask/FocusTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTaskApp.swift
+//  FocusTask
+//
+//  Created by Klesya Loha on 12/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FocusTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
